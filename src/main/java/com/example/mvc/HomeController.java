cd765cf3254f6135d7c74ae45853e0af8e78fb32
Ectimel <dtworek94@gmail.com>
2022-12-21 22:45:52 +0000

    trying to set it up...

---
+++ src/main/java/com/example/mvc/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/")
+    public String homePage() {
+        return "index";
+    }
+}
